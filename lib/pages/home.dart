6bd3332b8544a0edf10f549549d74d191d41f54a
skota <sriramkota@yahoo.com>
2018-12-06 07:30:21 -0500

    added static text to home page

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,7 +1,53 @@
 import 'package:flutter/material.dart';
 
+// top section - shows city, 
+// 1- city (Large font/ bold)
+// 2 - current state(cloudy rainy etc)
+// 3 - Temperature in F
+
+Widget _getHeader() {
+  
+  return Container(
+    padding: EdgeInsets.all(16.0),
+    child: Center(
+      child: Column(
+        children: <Widget>[
+        Text('Buford',style: 
+              TextStyle(fontSize: 50.0,
+              fontWeight: FontWeight.bold
+              ),),
+          Text('cloudy'),
+          new Text('25 \U00B0')
+        ],
+      ),
+    )
+  );
+
+}
+
+
+Widget _getDetail() {
+  return Text('Bottom section');
+}
+
+//Bottom section
+// Day of week
+// divider Line
+// large icon of weather ( rainy, cloudy etc)
+
+
 Widget homePage() {
-  return Text('Home Page');
+  return Center(
+    child: Column(
+      children: <Widget>[
+        _getHeader(),
+        Divider(
+          height: 18.0,
+        ),
+        _getDetail()
+      ],
+    ),
+  );
 }
 
 
